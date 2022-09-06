8a4ebe4fc59ba1dd900af980bac163e88bf35160
qthdf <110314206+qthdf@users.noreply.github.com>
2022-09-06 14:23:05 +0800

    Update heart.cpp

--- heart.cpp
+++ heart.cpp
@@ -168,6 +168,7 @@ namespace Microbit {
 		    particleSensor->nextSample(); //We're finished with this sample so move to next sample
 
 		  }
+		maxim_heart_rate_and_oxygen_saturation(irBuffer, bufferLength, redBuffer, &spo2_value, &validSPO2, &heartRate, &validHeartRate);
 		
 		
 		
