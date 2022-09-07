5f972e4a5fbaaff2836010293a808ab8ce2d2861
qthdf <110314206+qthdf@users.noreply.github.com>
2022-09-07 14:14:15 +0800

    Update heart.cpp

--- heart.cpp
+++ heart.cpp
@@ -161,17 +161,20 @@ namespace Microbit {
 		bufferLength=100;
 		
 		
-		for (int i = 0 ; i < 100 ; i++)
-		  {
-		  
+		for (byte i = 75; i < 100; i++)
+			{
+				while (particleSensor->available() == false) //do we have new data?
+					particleSensor->check(); //Check the sensor for new data
 
-		    redBuffer[i] = i;
-		    irBuffer[i] = i;
-		   
-
-		  }
+				
+				redBuffer[i] = particleSensor->getRed();
+				irBuffer[i] = particleSensor->getIR();
+				particleSensor->nextSample(); //We're finished with this sample so move to next sample
+				//After gathering 25 new samples recalculate HR and SP02
+				
+			}
 		
-		maxim_heart_rate_and_oxygen_saturation(&irBuffer,bufferLength, &redBuffer, &spo2_value, &validSPO2, &heartRate, &validHeartRate);
+		maxim_heart_rate_and_oxygen_saturation(irBuffer, bufferLength, redBuffer, &spo2_value, &validSPO2, &heartRate, &validHeartRate);
 		switch(type)
 			{
 				case 0:
